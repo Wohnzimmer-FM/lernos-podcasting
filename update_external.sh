fa273b7bda443a838411ee89f6fe421da38ec086
leonidlezner <kontakt@lezner.de>
2020-01-21 21:21:35 +0100

    Adding subtree

---
+++ update_external.sh
@@ -0,0 +1,3 @@
+#git subtree add --prefix=external/lernos-common --squash https://github.com/Wohnzimmer-FM/lernos-common.git master
+
+#git subtree pull --prefix=external/lernos-common --squash https://github.com/Wohnzimmer-FM/lernos-common.git master
