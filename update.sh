3080353535f4cf11ca770779939261b534c6ae7f
leonidlezner <kontakt@lezner.de>
2020-01-25 18:29:47 +0100

    Bash scripts are working

--- update.sh
+++ update.sh
@@ -1,3 +1,1 @@
-#git subtree add --prefix=external/lernos-common --squash https://github.com/Wohnzimmer-FM/lernos-common.git master
-
-git subtree pull --prefix=external/lernos-common --squash https://github.com/Wohnzimmer-FM/lernos-common.git master
+docker run --volume "$(pwd):/data" leonidlezner/skribos:0.7 ./tools/update.sh
